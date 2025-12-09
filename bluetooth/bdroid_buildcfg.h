b4fbd778b7ee3732a15ac6bc76dd014b7474862b
Dominggoes Isakh <drjisakh@gmail.com>
2025-12-09 21:48:07 +0100

    herolte:  Bluetooth: Moved BLE_VND_INCLUDED to runtime property bluetooth.ble.vnd.included
    Change-Id: I216e7e1c4166a8864045668e62755e761a6cf705

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -23,8 +23,6 @@
 #define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
 #define BTIF_HF_WBS_PREFERRED FALSE /* Don't prefer WBS    */
 
-#define BLE_VND_INCLUDED TRUE
-
 #define BTM_SCO_ENHANCED_SYNC_ENABLED FALSE
 
 #endif
